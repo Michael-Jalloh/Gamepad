207315b056ce0caea146906c52751966bd96ec39
Michael-Jalloh <michaeljalloh19@gmail.com>
2020-05-10 01:35:49 +0000

    Buttons - part 2

---
+++ Buttons - P2/Gamepad.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var dpad_sprite = {
+	"Left": preload("res://assets/BTN White/DPAD_left.png"),
+	"Right": preload("res://assets/BTN White/DPAD_right.png"),
+	"Up": preload("res://assets/BTN White/DPAD_up.png"),
+	"Down": preload("res://assets/BTN White/DPAD_down.png"),
+	"Normal": preload("res://assets/BTN White/DPAD_all.png"),
+}
+onready var dpad = $Dpad
+
+func _process(delta):
+	var data = "<"
+	var buttonState = 0
+	var normal = true
+	var counter = 0
+	for dbtn in get_tree().get_nodes_in_group("d-pad"):
+		if(dbtn.is_pressed()):
+			dpad.texture = dpad_sprite[dbtn.name]
+			normal = false
+			buttonState += 1 << counter
+		else:
+			buttonState += 0 << counter
+		counter += 1
+	if(normal):
+		dpad.texture = dpad_sprite["Normal"]
+	for btn in get_tree().get_nodes_in_group("buttons"):
+		buttonState += (1 if btn.is_pressed() else 0) << counter
+		counter += 1
+	for switch in get_tree().get_nodes_in_group("switch"):
+		buttonState += (1 if switch.pressed else 0) << counter
+		counter += 1
+	data += str(buttonState) + ">"
+	print(data)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Gamepad
+
+This the Gamepad repo for my [youtube tutorial](https://www.youtube.com/watch?v=TObNfKH5rkM&list=PLZoo5-KlHPHZFNMgh-ICXFeRsjk91LBDX).
+
+Its built on the [Godot 3.2 Engine](https://godotengine.org/download/)
